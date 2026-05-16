e648eeff00ddf22a866c01b8dcf4465a04d5cd25
Eshal Annisa Niagara <eshal.niagara@gmail.com>
2026-05-16 10:47:23 +0700

    login-update-esal

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,1 @@
+<h1>Dashboard Admin / Penjual 👑🌸</h1>
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,54 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
+  // Dashboard pengarah otomatis
+Route::get('/dashboard', function () {
+
+    if(auth()->user()->role == 'admin'){
+        return redirect('/admin/dashboard');
+    }
+
+    return redirect('/customer/dashboard');
+
+})->middleware(['auth'])->name('dashboard');
+
+
+
+  // Admin
+Route::middleware(['auth', 'role:admin'])->group(function () {
+
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+});
+
+
+
+  // Customer
+Route::middleware(['auth', 'role:customer'])->group(function () {
+
+    Route::get('/customer/dashboard', function () {
+        return view('customer.dashboard');
+    });
+
+});
+
+
+
+// Profile
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/profile', [ProfileController::class,'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class,'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class,'destroy'])
+        ->name('profile.destroy');
+
 });
 
 require __DIR__.'/auth.php';
